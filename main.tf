4d488df9aa175762416b5205f073fd33a4e8f765
Beau Galasso <beaugalasso@gmail.com>
2023-11-12 14:13:29 -0700

    Fix arns

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
+  target_group_arns   = module.blog_alb.target_groups
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
@@ -66,7 +66,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
   }
 
